6acdd5e4fad03af5bdd2dfd2994276e55eabba69
JCHernandezT <maxdbjc@gmail.com>
2025-11-06 17:24:44 -0500

    refactor(build): setup monorepo structure for React migration" -m "Prepara el proyecto para la migración a React/REST: - Crea el POM padre en la raíz para gestión de módulos. - Mueve el código de Spring Boot al módulo 'backend/' y lo configura como hijo. - Configura el .gitignore para soportar módulos de backend (Maven) y frontend (Node.js/React).

--- src/main/java/co/com/juan/poly/service/messages/NotificationService.java
+++
@@ -1,6 +0,0 @@
-package co.com.juan.poly.service.messages;
-
-public interface NotificationService {
-	void addInfoMessage(String msg);
-	void addErrorMessage(String msg);
-}
--- src/main/resources/static/js/delete-confirm.js
+++
@@ -1,8 +0,0 @@
-$(".to-delete").click(function(e) {
-	
-	var itemName = $(this).children(0).val();
-	
-	if (!confirm(DelMessage + " " + "\"" + itemName + "\"")) {
-		e.preventDefault();
-	}
-});
